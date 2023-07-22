abe8a2b0793b2f936074cd2ac64b5012db373545
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-22 14:31:36 +0200

    adding mui components

--- src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
@@ -1,9 +1,41 @@
+import { ShoppingCart } from '@mui/icons-material';
 import './styles.css';
+import { Button, FormControl, InputLabel, NativeSelect } from '@mui/material';
 
 export const MaterialUIBasicElements = () => {
   return (
-    <article>
-      <h1>Renderowanie z tablicy i obiketu</h1>
-    </article>
+    <div>
+      <Button variant="text">Text</Button>
+      <br /> <br />
+      <Button variant="contained" color="error">
+        Contained
+      </Button>
+      <br /> <br />
+      <Button variant="outlined">Outlined</Button>
+      <br /> <br />
+      <Button variant="contained" endIcon={<ShoppingCart />}>
+        Shop Now!
+      </Button>
+      <div>
+        <br /> <br />
+        <FormControl fullWidth>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            Age
+          </InputLabel>
+          <NativeSelect
+            defaultValue={30}
+            inputProps={{
+              name: 'age',
+              id: 'uncontrolled-native',
+            }}
+          >
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </NativeSelect>
+        </FormControl>
+      </div>
+      <div></div>
+    </div>
   );
 };
