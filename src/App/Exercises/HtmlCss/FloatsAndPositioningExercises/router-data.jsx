1e615dee26534d148f5559f631e63897828698f2
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-22 11:17:06 +0200

    float and positioning exercises added

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositioningExercises/router-data.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { FloatsAndPositioningExercises } from './index';
+
+export const blockRouterMetaData = {
+  path: 'float-and-positioning-exercises',
+  date: '22-07-2023',
+  linkLabel: 'Floats and positioning exercises',
+  blockNo: 17,
+  element: <FloatsAndPositioningExercises />,
+  tags: [
+    'z-index',
+    'positioning',
+    'css',
+    'float',
+    'absolute',
+    'fixed',
+    'relative',
+  ],
+};
