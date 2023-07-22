1a593eefe6727b10a62c670cb58190f63430b37c
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-22 13:44:48 +0200

    getting started with mui

---
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export const MaterialUIBasicElements = () => {
+  return (
+    <article>
+      <h1>Renderowanie z tablicy i obiketu</h1>
+    </article>
+  );
+};
---
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { MaterialUIBasicElements } from './MaterialUIBasicElements';
+
+export const MaterialUIBasicElementMetaData = {
+  path: 'material-ui-basic-elements',
+  date: '16-07-2023',
+  linkLabel: 'Material UI Basic Elements',
+  blockNo: 17,
+  element: <MaterialUIBasicElements />,
+  tags: ['Button', 'material-ui', 'React'],
+};
