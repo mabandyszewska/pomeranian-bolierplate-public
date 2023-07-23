5a8a07a03e29558cdf1252481d55eb5f28ab81fb
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-23 16:53:51 +0200

    empty values and comments explanation

---
+++ src/App/Exercises/Js/EmptyValues/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'empty-values-comments',
+  date: '23-07-2023',
+  linkLabel: 'Empty values and comments',
+  blockNo: 19,
+  element: <Exercise />,
+  tags: ['js', 'null', 'undefined', 'empty values'],
+};
