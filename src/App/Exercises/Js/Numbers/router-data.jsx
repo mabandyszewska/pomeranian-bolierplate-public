4146d6bca816c53d65722ddbc1c7b3182d67035c
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-23 12:22:17 +0200

    numbers, methods and operators explained

---
+++ src/App/Exercises/Js/Numbers/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'numbers',
+  date: '23-07-2023',
+  linkLabel: 'Numbers',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['numbers', 'operators', 'let', 'const'],
+};
