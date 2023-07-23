441eb8dc75bc3fc4c12efa21a5dd95ff48481820
Marta Bandyszewska <bandyszewska.marta@gmail.com>
2023-07-23 10:31:27 +0200

    boolean and oprators explaination

--- src/App/Exercises/ReactExercises/Cwiczenie1/router-data.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie1/router-data.jsx
@@ -3,7 +3,7 @@ import { Cwiczenie1 } from './Cwiczenie1';
 export const Cwiczenie1MetaData = {
   path: 'cwiczenie-1',
   date: '12-12-2023',
-  linkLabel: 'Cwiczenie 1',
+  linkLabel: '',
   blockNo: 1,
   element: <Cwiczenie1 />,
   tags: ['components', 'import', 'export'],
